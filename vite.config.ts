43c5ba115723373f7adbf60282161b6b6b75ef6d
immedha <immedha@outlook.com>
2025-05-28 20:45:39 -0700

    changed homepage in pkg.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // You can use '/' for user pages like immedha.github.io
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true
-  }
+  base: '/', // <- critical for GitHub user page
 })
